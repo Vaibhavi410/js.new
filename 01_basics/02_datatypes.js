d271080bd4c4aa6510de50153eb01ae74205700c
Vaibhavi <g.vaibhavi410@gmail.com>
2025-10-08 07:22:03 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello ") //we can use alert, prompt and confirm in browser only.. not in nodejs
+
+// console.log(3+3); // we can use console in both browser and nodejs
+
+let name="Vaibhavi"
+let age=19
+let isLoggedIn=false
+
+//number - 2 to power 53
+// bigint
+// string - "" '' ``
+// boolean - true/false
+// null - standalone value //it is an object in JS
+// undefined - standalone value //it is a type in JS
+// symbol - unique value
+
+//object - key value pairs
+console.log(typeof "Vaibhavi");
+//array - ordered list of values    
+//array is a type of object
+
+//JS is dynamically typed language - we dont have to specify datatype of variable at the time of declaration
+//datatype is decided based on the value assigned to it
+//we can change the datatype of variable at any point of time
+
+//JS is weakly typed language - we can perform operations on different datatypes        
