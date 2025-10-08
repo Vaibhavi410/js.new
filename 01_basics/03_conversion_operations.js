2521639b6f1aa97b8c4472042a8a4ac93454c376
Vaibhavi <g.vaibhavi410@gmail.com>
2025-10-08 07:19:29 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,46 @@
+// let score=33;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let score=false;
+// console.log(typeof score);
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+//  "33" ==> 33
+// "33abc" ==> NaN
+// true =1 false =0
+let isLoggedIn = 1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1 ==>True 0==> False
+// "Vaibhavi"==>true
+let someNumber=33;
+let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************Operations****************
+
+let value=3
+let negValue= -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1="hello"
+let str2=" Vaibhavi"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
